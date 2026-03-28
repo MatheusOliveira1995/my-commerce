c384b5718887136a0b7a12212220fe151d1b9aa6
MatheusOliveira1995 <mateusribeirooliveira@hotmail.com>
2026-03-28 18:39:24 -0300

    Refactor ProductHttpRepository to implement caching for product retrieval; optimize getAll method with cache management

--- src/domain/products/repositories/product-http-repository.ts
+++ src/domain/products/repositories/product-http-repository.ts
@@ -1,14 +1,43 @@
 import httpClient from "@/core/lib/http-client";
 import { Product, ProductsPageResult } from "@/domain/products/models/product";
-import { PRODUCT_ENDPOINTS, PRODUCTS_PER_PAGE } from "@/domain/products/constants";
+import {
+  PRODUCT_ENDPOINTS,
+  PRODUCTS_PER_PAGE,
+} from "@/domain/products/constants";
 import { IProductRepository } from "./product-repository.interface";
 
+const PRODUCTS_CACHE_TTL_MS = 1000 * 60 * 5;
+
 export class ProductHttpRepository implements IProductRepository {
+  private productsCache: Array<Product> | null = null;
+
+  private productsCacheExpiresAt = 0;
+
+  private pendingProductsRequest: Promise<Array<Product>> | null = null;
+
   async getAll(): Promise<Array<Product>> {
-    const { data } = await httpClient.get<Array<Product>>(
-      PRODUCT_ENDPOINTS.list,
-    );
-    return data;
+    const now = Date.now();
+
+    if (this.productsCache && now < this.productsCacheExpiresAt) {
+      return this.productsCache;
+    }
+
+    if (this.pendingProductsRequest) {
+      return this.pendingProductsRequest;
+    }
+
+    this.pendingProductsRequest = httpClient
+      .get<Array<Product>>(PRODUCT_ENDPOINTS.list)
+      .then((response) => {
+        this.productsCache = response.data;
+        this.productsCacheExpiresAt = Date.now() + PRODUCTS_CACHE_TTL_MS;
+        return response.data;
+      })
+      .finally(() => {
+        this.pendingProductsRequest = null;
+      });
+
+    return this.pendingProductsRequest;
   }
 
   async getPage(
